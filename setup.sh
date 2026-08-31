d3ca98760a7c9e6751065d90b23ecbf0904b39b2
angelacui1 <111831472+angelacu1@users.noreply.github.com>
2026-08-31 19:00:43 +0000

    add git config

--- setup.sh
+++ setup.sh
@@ -4,4 +4,8 @@ tmux_dst_path="$HOME/.tmux.conf"
 tmux_src_path="$HOME/dotfiles/.tmux.conf"
 ln -s "$tmux_src_path" "$tmux_dst_path"
 
+gitconf_dst_path="$HOME/.gitconfig"
+gitconf_src_path="$HOME/dotfiles/.gitconfig"
+ln -s "$gitconf_src_path" "$gitconf_dst_path"
+
 grep -qxF 'source ~/dotfiles/bash_setup.sh' ~/.bashrc || echo -e '\n\nsource ~/dotfiles/bash_setup.sh' >> ~/.bashrc
